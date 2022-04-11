c5284fb06a469eabbb99be401fb52db6e7205eca
Nur Akmal <2183322C@student.tp.edu.sg>
2022-04-12 02:15:03 +0800

    Update app.js

--- app.js
+++ app.js
@@ -30,6 +30,39 @@ app.use(function (req, res, next) {
 router.post('/newcustomer', (request, response) => {
 
     console.log(request.body)
+    console.log(request.body["email"])
+
+//     const query = `INSERT INTO
+//     \`cdptamrlytics.datasetFromTamr.known-users-2\` (Address,
+//       Name,
+//       Email,
+//       city,
+//       country,
+//       primaryKey,
+//       date)
+//   VALUES
+//     ('a','b','c','d','e',34,CURRENT_TIMESTAMP())`;
+
+//     // For all options, see https://cloud.google.com/bigquery/docs/reference/rest/v2/jobs/query
+//     const options = {
+//     query: query,
+//     // Location must match that of the dataset(s) referenced in the query.
+//     location: 'asia-southeast1',
+//     };
+
+//     // Run the query as a job
+//     const [job] = await bigquery.createQueryJob(options);
+//     //console.log(`Job ${job.id} started.`);
+
+//     // Wait for the query to finish
+//     const [rows] = await job.getQueryResults();
+
+//     // Print the results
+//     //console.log('Rows:');
+//     // rows.forEach(row => console.log(row));
+
+//     response.statusCode = 200;
+//     response.send(rows);
 
     response.statusCode = 200;
     response.send("Webhook received");
