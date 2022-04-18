eef1826e48ccee16c0e1cc2095c82b9fd9a5ba36
Nur Akmal <2183322C@student.tp.edu.sg>
2022-04-18 16:39:30 +0800

    Update app.js

--- app.js
+++ app.js
@@ -153,7 +153,7 @@ console.log(`connect_error due to ${err.message}`);
   
 
 var driver = neo4j.driver(
-  'bolt://neo4j.cdpdemodashboard.tk:7687',
+  'bolt://34.143.223.200:7687',
   neo4j.auth.basic('neo4j', 'dt')
 )
 var session = driver.session()
