d21a2bbee840987cc8be2791b28678df11da1806
Nur Akmal <2183322C@student.tp.edu.sg>
2022-04-18 16:41:38 +0800

    Update app.js

--- app.js
+++ app.js
@@ -153,8 +153,11 @@ console.log(`connect_error due to ${err.message}`);
   
 
 var driver = neo4j.driver(
-  'neo4j://34.143.223.200:7474',
-  neo4j.auth.basic('neo4j', 'dt')
+  'bolt://neo4j.cdpdemodashboard.tk:7687',
+  neo4j.auth.basic('neo4j', 'dt'), { encrypted: 'ENCRYPTION_ON',   trust: "TRUST_SYSTEM_CA_SIGNED_CERTIFICATES", trustedCertificates:['./ssl/neo4j.crt'] ,  logging: {
+    level: 'debug',
+    logger: (level, message) => console.log(level + ' ' + message)
+  }} 
 )
 var session = driver.session()
 session
