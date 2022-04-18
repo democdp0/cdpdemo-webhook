3f97b535ece475e819cf1bcb61334071264c46fd
Nur Akmal <2183322C@student.tp.edu.sg>
2022-04-18 22:30:29 +0800

    Update app.js

--- app.js
+++ app.js
@@ -143,6 +143,36 @@ router.get('/wordpressdb', async (request, response) => {
 
 });
 
+router.get('/usersfromecommerceb', async (request, response) => {
+
+  const query = `SELECT *
+  FROM \`cdptamrlytics.datasetFromTamr.usersFromEcommerceB\`
+  ORDER BY date DESC
+  LIMIT 5`;
+
+  // For all options, see https://cloud.google.com/bigquery/docs/reference/rest/v2/jobs/query
+  const options = {
+  query: query,
+  // Location must match that of the dataset(s) referenced in the query.
+  location: 'asia-southeast1',
+  };
+
+  // Run the query as a job
+  const [job] = await bigquery.createQueryJob(options);
+  //console.log(`Job ${job.id} started.`);
+
+  // Wait for the query to finish
+  const [rows] = await job.getQueryResults();
+
+  // Print the results
+  //console.log('Rows:');
+  // rows.forEach(row => console.log(row));
+
+  response.statusCode = 200;
+  response.send(rows);
+
+});
+
 
 router.get('/testtest', async (request, response) => {
   let config = {
