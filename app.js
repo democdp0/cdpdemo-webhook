2937b5d6ef47c70709ac17a16a170c7d6b634342
Nur Akmal <2183322C@student.tp.edu.sg>
2022-04-21 01:13:15 +0800

    Update app.js

--- app.js
+++ app.js
@@ -315,7 +315,12 @@ var options = {
 const server = http.createServer(app).listen(80);
 //const server = https.createServer(options, app).listen(443)
 
-const io = new Server(server);
+const io = new Server(server,{
+    cors: {
+      origin: ["https://www.cdpdemo.com","https://cdpdemo.com","https://api.cdpdemo.com"],
+      methods: ["GET", "POST"]
+    }
+  });
 
 io.on('connection', (socket) => {
     console.log('a user connected');
